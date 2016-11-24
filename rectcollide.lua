1b2dbe7852846baa5c0e2d77283911f2cd79bf03
Dominik <mail.dominik.s@googlemail.com>
2016-11-24 23:06:37 +0100

    converted all indentation to tabs and added a few comments

--- rectcollide.lua
+++ rectcollide.lua
@@ -7,10 +7,10 @@ rect1 = {
 	sy=20
 }
 rect2 = {
-    x=50,
-    y=50,
-    sx=20,
-    sy=20
+	x=50,
+	y=50,
+	sx=20,
+	sy=20
 }
 coll={false,0,0}
 
@@ -22,67 +22,70 @@ function drawrect(r,c)
 end
 
 function _draw()
-    cls()
-    if coll.overlap then c=8
-    else c=11 end
-    drawrect(rect1,c)
-    drawrect(rect2,c)
-    print(coll.xnorm..","..coll.ynorm,0,0)
+	cls()
+	if coll.overlap then c=8
+	else c=11 end
+	drawrect(rect1,c)
+	drawrect(rect2,c)
+	print(coll.xnorm..","..coll.ynorm,0,0)
 end
 
 function rectcollide(r1,r2)
-    -- calculate how much each point overlaps the other edge
-    local xovr1=(r1.x+r1.sx)-r2.x
-    local xovr2=(r2.x+r2.sx)-r1.x
-    local yovr1=(r1.y+r1.sy)-r2.y
-    local yovr2=(r2.y+r2.sy)-r1.y
+	-- calculate how much each point overlaps the other edge
+	-- this is best visualized, x and y overlap seperately!
+	local xovr1=(r1.x+r1.sx)-r2.x
+	local xovr2=(r2.x+r2.sx)-r1.x
+	local yovr1=(r1.y+r1.sy)-r2.y
+	local yovr2=(r2.y+r2.sy)-r1.y
 
-    -- take the smaller of the two horizontal overlaps as the normal
+	-- take the smaller of the two horizontal overlaps as the normal
 	local xnorm=-xovr1
 	if xovr2<xovr1 then
-        xnorm=xovr2
+		xnorm=xovr2
 	end
 
-    -- same for the vertical overlap
+	-- same for the vertical overlap
 	local ynorm=-yovr1
 	if yovr2<yovr1 then
-        ynorm=yovr2
+		ynorm=yovr2
 	end
-	
- return {
-    overlap=min(xovr1,xovr2)>0 and min(yovr1,yovr2)>0,
-    xnorm=xnorm,
-	ynorm=ynorm
-    }
+
+	return {
+		-- the rectangles only overlap if all of the points
+		-- overlaps the other edge positively!
+		overlap=min(xovr1,xovr2)>0 and min(yovr1,yovr2)>0,
+		xnorm=xnorm,
+		ynorm=ynorm
+	}
 end
 
 function _update()
 	local dt=time()-lasttime
 	lasttime=time()
-	
-    -- both rects are moveable!
-    if (btn(0,0)) rect1.x-=spd*dt
+
+	-- both rects are moveable!
+	if (btn(0,0)) rect1.x-=spd*dt
 	if (btn(1,0)) rect1.x+=spd*dt
 	if (btn(2,0)) rect1.y-=spd*dt
 	if (btn(3,0)) rect1.y+=spd*dt
 
-    if (btn(0,1)) rect2.x-=spd*dt
+	if (btn(0,1)) rect2.x-=spd*dt
 	if (btn(1,1)) rect2.x+=spd*dt
 	if (btn(2,1)) rect2.y-=spd*dt
 	if (btn(3,1)) rect2.y+=spd*dt
 
-    -- saves the collision in the global variable coll
+	-- saves the collision in the global variable coll
 	coll=rectcollide(rect1,rect2)
 
-    -- collision response: simply moves rect1
-    -- out of the collision by the normal
+	-- collision response: simply moves rect1
+	-- out of the collision by the normal
 	if coll.overlap then
-        -- only the smaller axis gets resolved here
-        -- the beauty of axis aligned 'physics' :)
-	    if abs(coll.xnorm) < abs(coll.ynorm) then
-	        rect1.x += coll.xnorm
-	    else
-	        rect1.y += coll.ynorm
-	    end
+		-- only the smaller axis gets resolved here
+		-- the beauty of axis aligned 'physics' :)
+		if abs(coll.xnorm) < abs(coll.ynorm) then
+			rect1.x += coll.xnorm
+		else
+			rect1.y += coll.ynorm
+		end
 	end
 end
